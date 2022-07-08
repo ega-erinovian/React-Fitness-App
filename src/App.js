07bc2af7cc9342d9f12aa458214edc1425bec42c
ega-erinovian <erinovian69@gmail.com>
2022-07-08 16:12:50 +0700

    added navbar & hero banner

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ import "./App.css";
 // Import Material UI
 import { Box } from "@mui/material";
 
+// Import components & pages
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
+
 const App = () => {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{width: {xl: '1488px'}}} m="auto">
         <Navbar />
         <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/exercise/:id" element={<ExerciseDetail />}/>
         </Routes>
+        <Footer/>
     </Box>
   )
 }
