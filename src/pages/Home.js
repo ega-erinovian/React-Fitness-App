c57070a7256658fc4528c8cd5966707c068e2f60
ega-erinovian <erinovian69@gmail.com>
2022-07-11 11:22:31 +0700

    added exercise card, search by name and body part, & pagination

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,11 +9,13 @@ const Home = () => {
   const [bodyPart, setBodyPart] = useState('all')
   const [exercises, setExercises] = useState([]);
 
+
+  console.log(bodyPart);
   return (
     <Box>
       <HeroBanner />
       <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
-      <Exercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} bodyPart={bodyPart} exercises={exercises} />
     </Box>
   )
 }
